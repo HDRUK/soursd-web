fee4790b6909bd30a25fc0c736fee71fbc052761
Peter Hammans <peter.hammans@hdruk.ac.uk>
2024-03-27 14:03:43 +0000

    feature: setup mui for nextjs

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,16 +1,25 @@
 import { makeServer } from "@/mocks/server";
 import { Footer, PageLayout } from "@/modules";
+import theme from "@/theme";
+import { AppCacheProvider } from "@mui/material-nextjs/v14-pagesRouter";
+import { ThemeProvider } from "@mui/material/styles";
 import type { AppProps } from "next/app";
 
 if (process.env.NEXT_PUBLIC_MOCK_DEV_SERVER === "true") {
   makeServer({ environment: "development" });
 }
 
-export default function App({ Component, pageProps }: AppProps) {
+export default function App(props: AppProps) {
+  const { Component, pageProps } = props;
+
   return (
-    <PageLayout>
-      <Component {...pageProps} />
-      <Footer />
-    </PageLayout>
+    <AppCacheProvider {...props}>
+      <ThemeProvider theme={theme}>
+        <PageLayout>
+          <Component {...pageProps} />
+          <Footer />
+        </PageLayout>
+      </ThemeProvider>
+    </AppCacheProvider>
   );
 }
---
+++ src/pages/_document.tsx
@@ -0,0 +1,38 @@
+import { makeServer } from "@/mocks/server";
+
+import {
+  DocumentHeadTags,
+  DocumentHeadTagsProps,
+  documentGetInitialProps,
+} from "@mui/material-nextjs/v14-pagesRouter";
+import {
+  DocumentContext,
+  DocumentProps,
+  Head,
+  Html,
+  Main,
+  NextScript,
+} from "next/document";
+
+if (process.env.NEXT_PUBLIC_MOCK_DEV_SERVER === "true") {
+  makeServer({ environment: "development" });
+}
+
+export default function Document(props: DocumentProps & DocumentHeadTagsProps) {
+  return (
+    <Html lang="en">
+      <Head>
+        <DocumentHeadTags {...props} />
+      </Head>
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  );
+}
+
+Document.getInitialProps = async (ctx: DocumentContext) => {
+  const finalProps = await documentGetInitialProps(ctx);
+  return finalProps;
+};
