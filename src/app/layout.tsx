5419a75740646db120b3f4b34dbce9841af45e87
Peter Hammans <peter.hammans@hdruk.ac.uk>
2024-03-27 14:10:53 +0000

    fix: missing eslint plugin

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,4 @@
 import { AppRouterCacheProvider } from "@mui/material-nextjs/v14-appRouter";
-import { ThemeProvider } from "@mui/material/styles";
-import theme from "../theme";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
