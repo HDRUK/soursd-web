1cc38a7f7a337867bc3a6214bbf198d1616c19fd
Peter Hammans <peter.hammans@hdruk.ac.uk>
2024-05-09 15:35:43 +0100

    feature: 2nd content panel

--- src/app/[locale]/(decorated)/page/page.tsx
+++ src/app/[locale]/(decorated)/page/page.tsx
@@ -5,11 +5,6 @@ import { Box, Typography } from "@mui/material";
 export default function Page() {
   return (
     <Box sx={{ overflow: "hidden", position: "relative" }}>
-      {/* <Carousel variant="hero">
-        {mockedCarouselSlides.map(carouselSlideProps => (
-          <CarouselSlide {...carouselSlideProps} />
-        ))}
-      </Carousel> */}
       <PageSection>
         <Typography variant="h4" sx={{ mt: 2 }}>
           [Page title]
@@ -17,40 +12,6 @@ export default function Page() {
       </PageSection>
       <PageSection sx={{ backgroundColor: "background2.main", mt: 1 }}>
         <HomepageStats />
-      </PageSection>
-
-      <PageSection>
-        {/* <FeatureBox color="background2">
-          <FeatureBoxInfo>
-            <Typography variant="h5">Quotes title content</Typography>
-            <Typography>This is some content</Typography>
-          </FeatureBoxInfo>
-          <FeatureBoxContent>
-            <Carousel>
-              <Quote
-                name="Researcher"
-                description="UKRI"
-                elevation={0}
-                sx={{ px: 9 }}>
-                Researcher quote goes here quote goes here quote
-              </Quote>
-              <Quote
-                name="Researcher"
-                description="UKRI"
-                elevation={0}
-                sx={{ px: 9 }}>
-                Researcher quote goes here quote goes here quote
-              </Quote>
-              <Quote
-                name="Researcher"
-                description="UKRI"
-                elevation={0}
-                sx={{ px: 9 }}>
-                Researcher quote goes here quote goes here quote
-              </Quote>
-            </Carousel>
-          </FeatureBoxContent>
-        </FeatureBox> */}
       </PageSection>
     </Box>
   );
