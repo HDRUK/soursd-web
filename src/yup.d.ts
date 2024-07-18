c4d2d1a8935bb95643ac5fe61324200fdd486361
Peter Hammans <peter.hammans@hdruk.ac.uk>
2024-07-18 16:05:54 +0100

    chore: remove comments

--- src/yup.d.ts
+++ src/yup.d.ts
@@ -8,13 +8,3 @@ declare module "yup" {
     ): StringSchema;
   }
 }
-
-// Completion screen
-
-// Tabs on profile, accreditations etc each has a status x or good
-
-// Left column -> status - profile complete or not complete also unique id is shown
-
-// Click on profile not complete takes you to first tab in workflow which has not been complete
-
-// First time profile is complete show unique id and a message in a modalClasses. Id should be copyable on click
