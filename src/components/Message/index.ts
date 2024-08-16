7cad532f72b0cf25c8d77152b99780eeb6cd391f
Peter Hammans <peter.hammans@hdruk.ac.uk>
2024-08-16 16:57:36 +0800

    chore: re-add export

--- src/components/Message/index.ts
+++ src/components/Message/index.ts
@@ -6,6 +6,6 @@ import MessageTitle from "./MessageTitle";
 export { Message, MessageInline, MessageContent, MessageTitle };
 
 export * from "./Message";
-// export * from "./MessageInline";
+export * from "./MessageInline";
 export * from "./MessageContent";
 export * from "./MessageTitle";
