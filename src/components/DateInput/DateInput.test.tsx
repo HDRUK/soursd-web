a00756a7fdcdb5b2a7291fd777ef749e154877b2
lewissaunders11 <lewis.saunders@hdruk.ac.uk>
2024-11-22 10:51:57 +0000

    change tests

--- src/components/DateInput/DateInput.test.tsx
+++ src/components/DateInput/DateInput.test.tsx
@@ -1,63 +1,28 @@
 import React from "react";
-import { render, screen, fireEvent } from "@testing-library/react";
+import { render, screen } from "@testing-library/react";
 import "@testing-library/jest-dom";
 import DateInput, { DateInputProps } from "./DateInput";
-
-jest.mock("@mui/x-date-pickers/DatePicker", () => ({
-  DatePicker: ({
-    label,
-    value,
-    onChange,
-  }: {
-    label: string;
-    value: Date | null;
-    onChange: (date: Date | null) => void;
-  }) => {
-    const id = "date-input";
-    return (
-      <div>
-        <label htmlFor={id}>{label}</label>
-        <input
-          data-testid={id}
-          id={id}
-          type="date"
-          value={value ? value.toISOString().split("T")[0] : ""}
-          onChange={e => onChange(new Date(e.target.value))}
-        />
-      </div>
-    );
-  },
-}));
 
 describe("DateInput Component", () => {
   const defaultProps: DateInputProps = {
     label: "Select a date",
     value: null,
     onChange: jest.fn(),
+    dataTestId: "date-input",
   };
 
   it("renders the component with a label", () => {
     render(<DateInput {...defaultProps} />);
     // Check if the label is rendered
-    expect(screen.getByText("Select a date")).toBeInTheDocument();
+    expect(screen.queryAllByText("Select a date")[0]).toBeInTheDocument();
   });
 
   it("renders the DatePicker inside the LocalizationProvider", () => {
     render(<DateInput {...defaultProps} />);
     // Check if the date input field is rendered
-    expect(screen.getByTestId("date-input")).toBeInTheDocument();
-  });
-
-  it("calls onChange when a new date is selected", () => {
-    const handleChange = jest.fn();
-    render(<DateInput {...defaultProps} onChange={handleChange} />);
-
-    // Simulate changing the date input value
-    const input = screen.getByTestId("date-input") as HTMLInputElement;
-    fireEvent.change(input, { target: { value: "2024-11-20" } });
-
-    // Check that the onChange function was called with the correct date
-    expect(handleChange).toHaveBeenCalledWith(new Date("2024-11-20"));
+    expect(
+      screen.getByRole("textbox", { name: "Choose date" })
+    ).toBeInTheDocument();
   });
 
   it("displays the correct value when a date is provided", () => {
@@ -65,15 +30,19 @@ describe("DateInput Component", () => {
     render(<DateInput {...defaultProps} value={date} />);
 
     // Ensure the input displays the correct date
-    const input = screen.getByTestId("date-input") as HTMLInputElement;
-    expect(input.value).toBe("2024-11-20");
+    const input = screen.getByRole("textbox", {
+      name: "Choose date, selected date is Nov 20, 2024",
+    }) as HTMLInputElement;
+    expect(input.value).toBe("11/20/2024");
   });
 
   it("displays an empty value when no date is provided", () => {
     render(<DateInput {...defaultProps} value={null} />);
 
     // Ensure the input is empty if no value is provided
-    const input = screen.getByTestId("date-input") as HTMLInputElement;
+    const input = screen.getByRole("textbox", {
+      name: "Choose date",
+    }) as HTMLInputElement;
     expect(input.value).toBe("");
   });
 });
