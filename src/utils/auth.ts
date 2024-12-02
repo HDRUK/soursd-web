083457d766237120395c0156a3d40bc55a5d76b6
Calum Macdonald <calmacx@gmail.com>
2024-12-02 14:41:24 +0000

    revert auth utils

--- src/utils/auth.ts
+++ src/utils/auth.ts
@@ -3,7 +3,6 @@
 import { Auth, User } from "@/types/application";
 import dayjs from "dayjs";
 import { cookies } from "next/headers";
-import { getRequest } from "@/services/requests";
 
 async function setAuthData(authData: Auth) {
   cookies().set("auth", JSON.stringify(authData), {
@@ -11,28 +10,9 @@ async function setAuthData(authData: Auth) {
   });
 }
 
-async function getMe(): Promise<User> {
-  const accessToken = cookies().get("access_token")?.value;
-
-  const response = await getRequest(
-    `${process.env.NEXT_PUBLIC_API_IP_URL}/auth/me`,
-    undefined,
-    {
-      headers: {
-        "content-type": "application/json;charset=UTF-8",
-        Authorization: `Bearer ${accessToken}`,
-      },
-    }
-  );
-  if (response.ok) {
-    const responseData = await response.json();
-    return responseData.data as User;
-  }
-  throw new Error("Failed to fetch user");
-}
-
 async function getAuthData(): Promise<Auth> {
   const authCookie = cookies().get("auth")?.value;
+
   return authCookie ? JSON.parse(decodeURI(authCookie)) : {};
 }
 
@@ -48,4 +28,4 @@ async function updateAuthUser(userData: Partial<User>) {
   });
 }
 
-export { getMe, getAuthData, setAuthData, updateAuthUser };
+export { getAuthData, setAuthData, updateAuthUser };
