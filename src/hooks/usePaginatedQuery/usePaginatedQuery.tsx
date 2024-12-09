f5f932692a3536f7bb5a7f4e7701b3b956d88b48
Calum Macdonald <calmacx@gmail.com>
2024-12-09 09:57:12 +0000

    create a new hook for paginated query

---
+++ src/hooks/usePaginatedQuery/usePaginatedQuery.tsx
@@ -0,0 +1,43 @@
+import { useState } from "react";
+import {
+  useQuery,
+  keepPreviousData,
+  UseQueryResult,
+} from "@tanstack/react-query";
+import { Paged } from "@/types/requests";
+
+type PaginatedQueryProps<T> = {
+  queryKeyBase: string;
+  queryFn: (page: number) => Promise<Paged<T>>;
+  initialPage?: number;
+};
+
+type PaginatedQueryReturn<T> = UseQueryResult<Paged<T>> & {
+  page: number;
+  setPage: React.Dispatch<React.SetStateAction<number>>;
+};
+
+const usePaginatedQuery = <T,>({
+  queryKeyBase,
+  queryFn,
+  initialPage = 1,
+}: PaginatedQueryProps<T>): PaginatedQueryReturn<T> => {
+  const [page, setPage] = useState<number>(initialPage);
+
+  const queryResult = useQuery({
+    queryKey: [queryKeyBase, page],
+    queryFn: () => queryFn(page),
+    placeholderData: keepPreviousData,
+  });
+
+  const { data: queryData, ...rest } = queryResult;
+
+  return {
+    ...rest,
+    ...(queryData ?? {}),
+    page,
+    setPage,
+  } as PaginatedQueryReturn<T>;
+};
+
+export default usePaginatedQuery;
