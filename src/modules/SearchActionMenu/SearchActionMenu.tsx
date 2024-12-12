be694711c9f46e029a8c23369615b7468cbea8c9
Calum Macdonald <calmacx@gmail.com>
2024-12-12 10:32:44 +0000

    forgot to add in files

---
+++ src/modules/SearchActionMenu/SearchActionMenu.tsx
@@ -0,0 +1,33 @@
+import React, { ReactNode } from "react";
+import { TextFieldProps } from "@mui/material/TextField";
+import FilterAltIcon from "@mui/icons-material/FilterAlt";
+import { Checkbox } from "@mui/material";
+import { ActionMenu, ActionMenuItem } from "../../components/ActionMenu";
+
+interface Action {
+  label: string;
+  icon?: ReactNode;
+  onClick: () => void;
+  checked: boolean;
+}
+
+type SortButtonProps = TextFieldProps & {
+  actions: Action[];
+};
+
+const SearchActionMenu = ({ actions }: SortButtonProps) => {
+  return (
+    <ActionMenu
+      icon={<FilterAltIcon />}
+      style={{ margin: "auto" }}
+      aria-label="sort-button">
+      {actions?.map(action => (
+        <ActionMenuItem key={action.label} onClick={action.onClick}>
+          <Checkbox checked={action.checked} />
+          {action?.icon} {action.label}
+        </ActionMenuItem>
+      ))}
+    </ActionMenu>
+  );
+};
+export default SearchActionMenu;
