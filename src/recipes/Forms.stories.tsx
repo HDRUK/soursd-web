7f27ff2365cc4d8c38222063437d744308859bc8
Peter Hammans <peter.hammans@hdruk.ac.uk>
2024-12-30 16:42:12 +0000

    feature: add stories

---
+++ src/recipes/Forms.stories.tsx
@@ -0,0 +1,82 @@
+import type { StoryObj } from "@storybook/react";
+
+import SyntaxHighlighter from "react-syntax-highlighter";
+
+const Forms = () => {
+  const codeString = `
+    interface FormFieldValues {
+      name: string;
+    }
+
+    const TestComponent = ({ queryState, onSubmit }) => {
+      const tForm = useTranslations('Form');
+
+      const defaultValues = {
+        name: ''
+      };
+
+      const schema = useMemo(
+        () =>
+          yup.object().shape({
+            name: yup.string().required(tForm("nameRequiredInvalid")),
+          }),
+        []
+      );
+
+      const handleSubmit = ({ name }: FormFieldValues) => {
+        onSubmit({ name });
+      }
+
+      return (
+        <Form
+          schema={schema}
+          defaultValues={defaultValues}
+          onSubmit={handleSubmit}
+          autoComplete="off">
+          {({ formState: { errors } }) => (
+            <FormSection>
+              <Grid container rowSpacing={3}>
+                <Grid item xs={12}>
+                  <FormControlHorizontal
+                    id="name"
+                    error={errors.name}
+                    renderField={fieldProps => (
+                      <FormField component={TextField} {...fieldProps} />
+                    )}
+                  />
+                </Grid>
+              </Grid>
+              </FormSection>
+              <FormActions>
+                <LoadingButton
+                  type="submit"
+                  endIcon={<SaveIcon />}
+                  loading={queryState.isLoading}>
+                  {tProfile("submitButton")}
+                </LoadingButton>
+              </FormActions>
+            </>
+          )}
+        </Form>
+      );
+    }
+  `;
+
+  return (
+    <SyntaxHighlighter language="typescript">{codeString}</SyntaxHighlighter>
+  );
+};
+
+const meta = {
+  title: "recipes/Forms",
+  component: Forms,
+  tags: ["autodocs"],
+};
+
+export default meta;
+
+type Story = StoryObj<typeof meta>;
+
+export const Example: Story = {
+  args: {},
+};
