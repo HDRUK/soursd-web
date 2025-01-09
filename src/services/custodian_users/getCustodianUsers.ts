ce083bc5fcaf5272c6ed37bb067931dd651c69cd
Calum Macdonald <calmacx@gmail.com>
2025-01-09 12:22:30 +0000

    fix lint errors

--- src/services/custodian_users/getCustodianUsers.ts
+++ src/services/custodian_users/getCustodianUsers.ts
@@ -6,12 +6,12 @@ export default async (
   custodianId: string | number | undefined,
   messages?: ResponseTranslations
 ): Promise<ResponseJson<GetCustodiansUsersResponse>> => {
-  //this is a temporary placeholder... this will have to be fixed soon
+  // this is a temporary placeholder... this will have to be fixed soon
   // - need to return custodian users FOR a custodian
   // - as of now, the BE just returns all custodian users
   console.log(custodianId);
   const response = await getRequest(
-    `${process.env.NEXT_PUBLIC_API_V1_URL}/custodian_users`, ///${custodianId}`,
+    `${process.env.NEXT_PUBLIC_API_V1_URL}/custodian_users`, // note - update in some way? ${custodianId}`,
     undefined,
     {
       headers: {
