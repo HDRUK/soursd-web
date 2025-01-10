279c2ab1ca9f9244b974af50153f87adf26c2ecd
Calum Macdonald <calmacx@gmail.com>
2025-01-10 14:15:17 +0000

    add guidance placeholder

--- src/app/[locale]/(logged-in)/data-custodian/profile/[tabId]/components/Configuration/Configuration.tsx
+++ src/app/[locale]/(logged-in)/data-custodian/profile/[tabId]/components/Configuration/Configuration.tsx
@@ -3,11 +3,14 @@ import { getRules } from "@/services/rules";
 import { Box } from "@mui/material";
 import ListInfoItem from "@/components/ListInfoItem";
 import { useTranslations } from "next-intl";
+import { PageGuidance } from "@/modules";
+import { mockedPersonalDetailsGuidanceProps } from "@/mocks/data/cms";
 
 const NAMESPACE_TRANSLATION_RULES = "Rules";
 
 const Configuration = () => {
   const t = useTranslations(NAMESPACE_TRANSLATION_RULES);
+
   const { data: rules } = useQuery({
     queryKey: ["getUserApprovedProjects"],
     queryFn: () =>
@@ -18,15 +21,17 @@ const Configuration = () => {
       }),
   });
   return (
-    <Box display="flex" justifyContent="center" padding={2}>
-      <Box display="flex" flexDirection="column" gap={0} maxWidth={1000}>
-        {rules?.data.nodes.map((node, index) => (
-          <ListInfoItem key={`info-box-${node.name}`} index={index + 1}>
-            {`${t(`${node.name}.title`)}: ${t(`${node.name}.description`)}`}
-          </ListInfoItem>
-        ))}
+    <PageGuidance {...mockedPersonalDetailsGuidanceProps}>
+      <Box display="flex" justifyContent="center" padding={2}>
+        <Box display="flex" flexDirection="column" gap={0} maxWidth={1000}>
+          {rules?.data.nodes.map((node, index) => (
+            <ListInfoItem key={`info-box-${node.name}`} index={index + 1}>
+              {`${t(`${node.name}.title`)}: ${t(`${node.name}.description`)}`}
+            </ListInfoItem>
+          ))}
+        </Box>
       </Box>
-    </Box>
+    </PageGuidance>
   );
 };
 
