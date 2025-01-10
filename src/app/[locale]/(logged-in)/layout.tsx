29deb123d155f7751fc3885102fb1297b0de31dc
lewissaunders11 <lewis.saunders@hdruk.ac.uk>
2025-01-10 07:28:25 +0000

    test

--- src/app/[locale]/(logged-in)/layout.tsx
+++ src/app/[locale]/(logged-in)/layout.tsx
@@ -28,7 +28,7 @@ async function validateAccessToken(
       },
     }
   );
-
+  
   if (response.ok) {
     return response.json();
   }
@@ -56,7 +56,6 @@ export default function Layout({ children, params: { locale } }: LayoutProps) {
   useEffect(() => {
     const performAuthCheck = async () => {
       const user = await validateAccessToken(pathname, router);
-
       if (!user?.data) {
         throw new Error("Unauthorised 401");
       }
