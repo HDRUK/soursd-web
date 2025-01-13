7069a3f97354caf58e1adb9983f292970d7bc22c
lewissaunders11 <lewis.saunders@hdruk.ac.uk>
2025-01-13 13:12:40 +0000

    initial implementation

--- src/app/[locale]/(logged-in)/organisation/profile/[tabId]/components/Delegates/Delegates.tsx
+++ src/app/[locale]/(logged-in)/organisation/profile/[tabId]/components/Delegates/Delegates.tsx
@@ -1,21 +1,39 @@
 "use client";
 
 import { mockedManageDelegatesGuidance } from "@/mocks/data/cms";
-import { PageGuidance } from "@/modules";
-import DelegatesForm, { DelegatesFormValues } from "./DelegatesForm";
-import { patchOrganisation, PatchOrganisationPayload } from "@/services/organisations";
+import { PageGuidance, PageSection } from "@/modules";
+import {
+  getOrganisationUsers,
+  patchOrganisation,
+  PatchOrganisationPayload,
+} from "@/services/organisations";
 import { useStore } from "@/data/store";
 import { useMutation, useQuery } from "@tanstack/react-query";
 import Results from "@/components/Results";
 import { useTranslations } from "next-intl";
 import { getCustodianUsers } from "@/services/custodian_users";
 import { formatShortDate } from "@/utils/date";
-import { Card, CardContent, Typography, IconButton } from "@mui/material";
+import {
+  Card,
+  CardContent,
+  Typography,
+  IconButton,
+  Pagination,
+} from "@mui/material";
 import { Box } from "@mui/system";
 import CreateOutlinedIcon from "@mui/icons-material/CreateOutlined";
 import DeleteForeverOutlinedIcon from "@mui/icons-material/DeleteForeverOutlined";
 import PersonOutlineOutlinedIcon from "@mui/icons-material/PersonOutlineOutlined";
 import Icon from "@/components/Icon";
+import usePaginatedQuery from "@/hooks/usePaginatedQuery";
+import ApplicationLink from "@/components/ApplicationLink";
+import ResultsCard from "@/components/ResultsCard";
+import UserRegisteredStatus from "@/components/UserRegisteredStatus";
+import { DecoupleIcon } from "@/consts/icons";
+import { isRegistered } from "@/utils/user";
+import UserModal from "../UserModal";
+import usePatchOrganisation from "../../hooks/usePatchOrganisation";
+import DelegatesForm, { DelegatesFormValues } from "./DelegatesForm";
 
 const NAMESPACE_PROFILE_ORGANISATION = "ProfileOrganisation";
 
@@ -29,43 +47,42 @@ export default function Delegates() {
     };
   });
 
-  const custodian = useStore(state => state.getCustodian());
-
   const {
-    isError: isGetCustodiansError,
-    isLoading: isGetCustodiansLoading,
-    data: custodiansData,
-  } = useQuery({
-    queryKey: ["getCustodianUsers", custodian?.id],
-    queryFn: () => getCustodianUsers(),
-  });
-
-  const {
-    mutateAsync: mutateUpdateAsync,
     isError,
     isPending: isLoading,
     error,
-  } = useMutation({
-    mutationKey: ["patchCustodian", organisation?.id],
-    mutationFn: (payload: PatchOrganisationPayload) =>
-      patchOrganisation(organisation?.id, payload, {
-        error: {
-          message: "submitError",
-        },
-      }),
+    onSubmit,
+  } = usePatchOrganisation({
+    id: organisation?.id,
+    organisation,
+    setOrganisation,
   });
 
-  const handleSubmit = async (fields: DelegatesFormValues) => {
-    const payload = { ...organisation, ...fields };
+  const {
+    isError: isGetUsersError,
+    isLoading: isGetUsersLoading,
+    data: usersData,
+    last_page,
+    page,
+    setPage,
+  } = usePaginatedQuery({
+    queryKeyBase: ["getOrganisationUsers", organisation?.id],
+    queryFn: queryParams => {
+      return getOrganisationUsers(organisation?.id, queryParams, {
+        error: {
+          message: "getUsersError",
+        },
+      });
+    },
+    enabled: !!organisation,
+  });
 
-    await mutateUpdateAsync(payload);
+  const delegates = usersData?.filter(user => user.is_delegate === 1);
 
-    setOrganisation(payload);
-  };
   return (
     <PageGuidance {...mockedManageDelegatesGuidance}>
       <DelegatesForm
-        onSubmit={handleSubmit}
+        onSubmit={onSubmit}
         queryState={{
           isError,
           isLoading,
@@ -73,85 +90,69 @@ export default function Delegates() {
         }}
       />
       <Results
-        noResultsMessage={t("noDelegates")}
-        errorMessage={t("getError")}
+        noResultsMessage={t("noResults")}
+        errorMessage={t.rich("getError", {
+          applicationLink: ApplicationLink,
+        })}
         queryState={{
-          isLoading: isGetCustodiansLoading,
-          isError: isGetCustodiansError,
+          isLoading: isGetUsersLoading,
+          isError: isGetUsersError,
         }}>
-        {custodiansData?.data.map(custodianUser => {
-          const { first_name, last_name, created_at, email } = custodianUser;
+        {delegates?.map(user => {
+          const { first_name, last_name, created_at, email } = user;
 
           return (
-            <Card sx={{ mb: 1 }} role="listitem" key={email}>
-              <CardContent>
-                <Box
-                  sx={{
-                    display: "flex",
-                    flexDirection: {
-                      xs: "column",
-                      md: "row",
-                    },
-                    width: "100%",
-                    gap: {
-                      xs: 1,
-                      md: 2,
-                    },
-                    alignItems: {
-                      md: "center",
-                    },
-                  }}>
-                  <Box sx={{ display: "flex", gap: 2 }}>
-                    <Icon size="xlarge">
-                      <PersonOutlineOutlinedIcon />
-                    </Icon>
-                    <div>
-                      <Typography variant="h6">
-                        {first_name} {last_name}
-                      </Typography>
-                      {/* Will be read from db */}
-                      <Typography>Administrator</Typography>
-                    </div>
-                  </Box>
-                  <Box
-                    sx={{
-                      flexGrow: 1,
-                      textAlign: {
-                        md: "right",
-                      },
-                    }}>
-                    <Typography color="caption.main">
-                      {t("addedOn", {
-                        date: formatShortDate(created_at),
-                      })}
-                    </Typography>
-                    <Typography color="caption.main">
-                      {t("lastLoggedIn", {
-                        date: formatShortDate(),
-                      })}
-                    </Typography>
-                  </Box>
-                  <Box>
-                    <IconButton
-                      size="small"
-                      aria-label="Edit user"
-                    
-                      >
-                      <CreateOutlinedIcon sx={{ color: "default.main" }} />
-                    </IconButton>
-                    <IconButton
-                      size="small"
-                      aria-label="Delete user"
-                      >
-                      <DeleteForeverOutlinedIcon sx={{ color: "error.main" }} />
-                    </IconButton>
-                  </Box>
-                </Box>
-              </CardContent>
-            </Card>
+            <ResultsCard
+              icon={
+                <Icon size="xlarge">
+                  <PersonOutlineOutlinedIcon />
+                </Icon>
+              }
+              content={
+                <>
+                  <Typography variant="body1" sx={{ fontWeight: "bolder" }}>
+                    {first_name} {last_name}
+                  </Typography>
+                  <Typography>{email}</Typography>
+                </>
+              }
+              details={
+                <>
+                  <Typography color="caption.main">
+                    {t("invitedOn", {
+                      date: formatShortDate(created_at),
+                    })}
+                  </Typography>
+                  <UserRegisteredStatus registered={isRegistered(user)} />
+                </>
+              }
+              actions={
+                <IconButton
+                  size="small"
+                  color="inherit"
+                  aria-label="icon-button">
+                  <DecoupleIcon />
+                </IconButton>
+              }
+            />
           );
         })}
       </Results>
+      <PageSection
+        sx={{
+          flexGrow: 1,
+          display: "flex",
+          justifyContent: "center",
+        }}>
+        <Pagination
+          isLoading={isGetUsersLoading}
+          page={page}
+          count={last_page}
+          onChange={(e: React.ChangeEvent<unknown>, page: number) =>
+            setPage(page)
+          }
+        />
+      </PageSection>
     </PageGuidance>
-  )
+  );
 }
