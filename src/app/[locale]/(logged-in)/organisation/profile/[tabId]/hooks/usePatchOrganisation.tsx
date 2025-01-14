c6d53e589c8f92272dc5cdd180f270a2de7efc67
Calum Macdonald <calmacx@gmail.com>
2025-01-14 16:00:28 +0000

    remove this stuff

--- src/app/[locale]/(logged-in)/organisation/profile/[tabId]/hooks/usePatchOrganisation.tsx
+++ src/app/[locale]/(logged-in)/organisation/profile/[tabId]/hooks/usePatchOrganisation.tsx
@@ -29,7 +29,9 @@ const usePatchOrganisation = ({
   });
 
   const onSubmit = async (fields: Partial<PatchOrganisationPayload>) => {
+    console.log(fields);
     const payload = { ...fields };
+    console.log(payload);
     await mutation.mutateAsync(payload);
     if (organisation && setOrganisation) {
       setOrganisation({
