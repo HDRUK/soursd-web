b166347507eaaa928fbc845a4f2bc2e84b897329
lewissaunders11 <lewis.saunders@hdruk.ac.uk>
2025-01-28 16:20:19 +0000

    remove conslle log

--- src/app/[locale]/(logged-in)/user/profile/[tabId]/components/Training/Training.tsx
+++ src/app/[locale]/(logged-in)/user/profile/[tabId]/components/Training/Training.tsx
@@ -243,7 +243,7 @@ export default function Training() {
     { name: "awarded_at", component: DateInput },
     { name: "expires_at", component: DateInput },
   ];
-  console.log(user, histories?.training);
+  
   return (
     <PageGuidance {...mockedPersonalDetailsGuidanceProps}>
       <Form onSubmit={handleDetailsSubmit} schema={schema} {...formOptions}>
