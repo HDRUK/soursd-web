4a12e8909ff7bcf1f270d5c4166893c22126c6b2
Calum Macdonald <calmacx@gmail.com>
2025-02-05 09:57:57 +0000

    fixes for subtab rendering

--- src/app/[locale]/(logged-in)/organisation/profile/[tabId]/[subTabId]/components/SubsTabContents/SubTabsContents.tsx
+++ src/app/[locale]/(logged-in)/organisation/profile/[tabId]/[subTabId]/components/SubsTabContents/SubTabsContents.tsx
@@ -2,7 +2,13 @@
 
 import { useStore } from "@/data/store";
 import { notFound } from "next/navigation";
-import { DetailsPageSubTabs, UserAdminPageSubTabs } from "../../../consts/tabs";
+import {
+  DetailsPageSubTabs,
+  UserAdminPageSubTabs,
+  getSubTabs,
+  PageTabs,
+  PageSubTabs,
+} from "../../../consts/tabs";
 import NameAndAddress from "../NameAndAddress";
 import DigitalIdentifiers from "../DigitalIdentifiers";
 import SectorSiteAndWebsite from "../SectorSiteAndWebsite";
@@ -12,36 +18,55 @@ import Delegates from "../Delegates";
 import Users from "../Users";
 
 interface TabsContentsProps {
+  tabId: string;
   subTabId: string;
 }
 
-export default function SubTabsContents({ subTabId }: TabsContentsProps) {
+export default function SubTabsContents({
+  tabId,
+  subTabId,
+}: TabsContentsProps) {
   const [user, organisation] = useStore(state => [
     state.getUser(),
     state.getOrganisation(),
   ]);
 
-  if (!user || !organisation) notFound();
+  const availableSubTabs = getSubTabs(tabId as PageTabs) || [];
 
-  return (
-    <>
-      {subTabId === DetailsPageSubTabs.NAME_AND_ADDRESS && <NameAndAddress />}
-      {subTabId === DetailsPageSubTabs.DIGITAL_IDENTIFIERS && (
-        <DigitalIdentifiers />
-      )}
-      {subTabId === DetailsPageSubTabs.SECTOR_SITE_AND_WEBSITE && (
-        <SectorSiteAndWebsite />
-      )}
-      {subTabId === DetailsPageSubTabs.SUBSIDIARIES && <Subsidiaries />}
-      {subTabId === DetailsPageSubTabs.SECURITY_COMPLIANCE && (
-        <SecurityCompliance />
-      )}
-      {subTabId === UserAdminPageSubTabs.DELEGATE_ADMINISTRATION && (
-        <Delegates />
-      )}
-      {subTabId === UserAdminPageSubTabs.EMPLOYEE_STUDENT_ADMINISTRATION && (
-        <Users />
-      )}
-    </>
-  );
+  if (
+    !user ||
+    !organisation ||
+    !availableSubTabs.includes(subTabId as PageSubTabs)
+  )
+    notFound();
+
+  let content = null;
+
+  switch (subTabId) {
+    case DetailsPageSubTabs.NAME_AND_ADDRESS:
+      content = <NameAndAddress />;
+      break;
+    case DetailsPageSubTabs.DIGITAL_IDENTIFIERS:
+      content = <DigitalIdentifiers />;
+      break;
+    case DetailsPageSubTabs.SECTOR_SITE_AND_WEBSITE:
+      content = <SectorSiteAndWebsite />;
+      break;
+    case DetailsPageSubTabs.SUBSIDIARIES:
+      content = <Subsidiaries />;
+      break;
+    case DetailsPageSubTabs.SECURITY_COMPLIANCE:
+      content = <SecurityCompliance />;
+      break;
+    case UserAdminPageSubTabs.DELEGATE_ADMINISTRATION:
+      content = <Delegates />;
+      break;
+    case UserAdminPageSubTabs.EMPLOYEE_STUDENT_ADMINISTRATION:
+      content = <Users />;
+      break;
+    default:
+      content = null;
+  }
+
+  return <>{content}</>;
 }
