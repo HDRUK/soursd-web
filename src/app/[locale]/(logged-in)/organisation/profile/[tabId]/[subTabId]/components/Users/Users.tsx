33e3bd6049820bfa7c94a773f2e2ce4cbb3b9d57
Calum Macdonald <calmacx@gmail.com>
2025-02-20 13:48:01 +0000

    fix for user state

--- src/app/[locale]/(logged-in)/organisation/profile/[tabId]/[subTabId]/components/Users/Users.tsx
+++ src/app/[locale]/(logged-in)/organisation/profile/[tabId]/[subTabId]/components/Users/Users.tsx
@@ -1,25 +1,11 @@
-import ContactLink from "@/components/ContactLink";
-import Icon from "@/components/Icon";
 import Pagination from "@/components/Pagination";
-import Results from "@/components/Results";
-import ResultsCard from "@/components/ResultsCard";
-import UserRegisteredStatus from "@/components/UserRegisteredStatus";
 import { useStore } from "@/data/store";
 import usePaginatedQuery from "@/hooks/usePaginatedQuery";
 import { PageBody, PageSection } from "@/modules";
 import SearchBar from "@/modules/SearchBar";
 import { getOrganisationRegistries } from "@/services/organisations";
 import { formatShortDate } from "@/utils/date";
-import { isRegistered } from "@/utils/user";
-import AddCircleOutlineOutlinedIcon from "@mui/icons-material/AddCircleOutlineOutlined";
-import PersonOutlineOutlinedIcon from "@mui/icons-material/PersonOutlineOutlined";
-import {
-  Box,
-  Button,
-  Checkbox,
-  FormControlLabel,
-  Typography,
-} from "@mui/material";
+import { Box, Button, Checkbox, FormControlLabel } from "@mui/material";
 import { useTranslations } from "next-intl";
 import { useState } from "react";
 import CheckCircleIcon from "@mui/icons-material/CheckCircle";
@@ -30,7 +16,7 @@ import { ColumnDef, CellContext } from "@tanstack/react-table";
 import UserModal from "../UserModal";
 import UserBulkInvite from "../UserBulkInvite";
 import { User } from "@/types/application";
-import Table from "@/modules/Table";
+import Table from "@/components/Table";
 import Markdown from "@/components/Markdown";
 
 const NAMESPACE_TRANSLATION_PROFILE = "ProfileOrganisation";
@@ -41,13 +27,13 @@ export default function Users() {
   const organisation = useStore(state => state.config.organisation);
 
   const {
-    isLoading: isGetUsersLoading,
     data: usersData,
     refetch: refetchOrganisationUsers,
     last_page,
     page,
     setPage,
     updateQueryParam,
+    ...userDataQueryState
   } = usePaginatedQuery({
     queryKeyBase: ["getOrganisationRegistries", organisation?.id],
     defaultQueryParams: {
@@ -88,7 +74,7 @@ export default function Users() {
     {
       accessorKey: "id",
       header: <Checkbox value={true} />,
-      cell: info => <Checkbox value={true} />,
+      cell: () => <Checkbox value={true} />,
     },
     {
       accessorKey: "name",
@@ -146,53 +132,9 @@ export default function Users() {
           last_page={last_page}
           data={usersData || []}
           columns={columns}
+          queryState={userDataQueryState}
         />
 
-        {/*<Results
-          noResultsMessage={t("noResults")}
-          errorMessage={t.rich("getError", {
-            contactLink: ContactLink,
-          })}
-          queryState={{
-            isLoading: isGetUsersLoading,
-            isError: isGetUsersError,
-          }}
-          count={usersData?.length}>
-          {usersData?.map(user => {
-            const { first_name, last_name, created_at, email } = user;
-
-            return (
-              <ResultsCard
-                icon={
-                  <Icon size="xlarge">
-                    <PersonOutlineOutlinedIcon />
-                  </Icon>
-                }
-                content={
-                  <>
-                    <Typography variant="body1" sx={{ fontWeight: "bolder" }}>
-                      {first_name} {last_name}
-                    </Typography>
-                    <Typography>{email}</Typography>
-                  </>
-                }
-                details={
-                  <>
-                    <Typography color="caption.main">
-                      {t("invitedOn", {
-                        date: formatShortDate(created_at),
-                      })}
-                    </Typography>
-                    <UserRegisteredStatus registered={isRegistered(user)} />
-                  </>
-                }
-                actions={
-                  
-                }
-              />
-            );
-          })}
-        </Results>*/}
         <Box sx={{ display: "flex", gap: 2, flexDirection: "row" }}>
           <div>
             <Button
@@ -212,7 +154,7 @@ export default function Users() {
           justifyContent: "center",
         }}>
         <Pagination
-          isLoading={isGetUsersLoading}
+          isLoading={userDataQueryState.isLoading}
           page={page}
           count={last_page}
           onChange={(e: React.ChangeEvent<unknown>, page: number) =>
