13c137ac102d5cfa59dfc7ee290b077195ba14a7
Calum Macdonald <calmacx@gmail.com>
2025-02-24 13:40:13 +0000

    forgot to add story changes

--- src/components/AddressForm/AddressForm.stories.tsx
+++ src/components/AddressForm/AddressForm.stories.tsx
@@ -1,5 +1,7 @@
 import type { Meta, StoryObj } from "@storybook/react";
 import { useForm, FormProvider } from "react-hook-form";
+import * as yup from "yup";
+import { yupResolver } from "@hookform/resolvers/yup";
 import AddressForm, { AddressFormProps } from "./AddressForm";
 
 const meta = {
@@ -13,7 +15,32 @@ export default meta;
 type Story = StoryObj<typeof meta>;
 
 const FormWrapper = ({ children }: { children: React.ReactNode }) => {
-  const methods = useForm();
+  const schema = yup.object().shape({
+    address: yup.object().shape({
+      address_1: yup.string().required("address1RequiredInvalid"),
+      address_2: yup.string().nullable(),
+      town: yup.string().required("townRequiredInvalid"),
+      county: yup.string().nullable(),
+      country: yup.string().nullable(),
+      postcode: yup.string().required("postcodeRequiredInvalid"),
+    }),
+  });
+
+  const formOptions = {
+    defaultValues: {
+      address: {
+        address_1: "",
+        address_2: "",
+        town: "",
+        county: "",
+        country: "United Kingdom",
+        postcode: "",
+      },
+    },
+    resolver: yupResolver(schema),
+  };
+
+  const methods = useForm(formOptions);
   return <FormProvider {...methods}>{children}</FormProvider>;
 };
 
