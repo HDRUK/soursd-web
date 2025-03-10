a59c67c640eed3728094f4e205cbb95312f705f1
Sam Cox <sam.cox@nottingham.ac.uk>
2025-03-10 11:59:32 +0000

    Remove tabId folder and slim down subpage of broken code.

--- src/app/[locale]/(logged-in)/user/profile/components/ProjectsSafePeople/ProjectsSafePeople.tsx
+++ src/app/[locale]/(logged-in)/user/profile/components/ProjectsSafePeople/ProjectsSafePeople.tsx
@@ -20,96 +20,9 @@ const NAMESPACE_TRANSLATION_PROFILE = "Profile";
 const NAMESPACE_TRANSLATION_APPLICATION = "Application";
 
 export default function ProjectsSafePeople({ id }: ProjectsSafePeopleProps) {
-  const {
-    data: usersData,
-    updateQueryParam,
-    last_page,
-    total,
-    setPage,
-    handleFieldToggle,
-    queryParams,
-    ...queryState
-  } = useProjectUsersQuery(id);
-  const t = useTranslations(NAMESPACE_TRANSLATION_PROFILE);
-  const tApplication = useTranslations(NAMESPACE_TRANSLATION_APPLICATION);
-  const routes = useStore(state => state.getApplication().routes);
-
-  const getUsersFromResponse = (usersData: ProjectUser[]) => {
-    const users: FilteredUser[] = [];
-
-    usersData?.forEach(({ registry: { user, organisations } }) => {
-      organisations?.forEach(({ organisation_name }) => {
-        users.push({
-          organisation_name,
-          ...user,
-          project_status: "Live",
-          project_role: "Data analyst",
-        });
-      });
-    });
-
-    return users;
-  };
-
-  const users = getUsersFromResponse(usersData);
-
-  const filterActions = [
-    {
-      label: tApplication("userStatusLive"),
-      onClick: () => handleFieldToggle("status", ["live", ""]),
-      checked: queryParams.status === "live",
-    },
-  ];
-
-  const columns: ColumnDef<FilteredUser>[] = [
-    {
-      cell: row =>
-        renderUserNameCell(row, routes.profileCustodianUsersIdentity.path),
-      accessorKey: "name",
-      header: tApplication("name"),
-    },
-    {
-      accessorKey: "project_role",
-      header: tApplication("projectRole"),
-    },
-    {
-      accessorKey: "organisation_name",
-      header: tApplication("organisationName"),
-    },
-    {
-      accessorKey: "project_status",
-      header: tApplication("projectStatus"),
-    },
-    {
-      header: tApplication("actions"),
-    },
-  ];
-
   return (
     <>
-      <Box component="form" role="search">
-        <SearchBar
-          updateQueryParam={(text: string) => updateQueryParam("name[]", text)}
-          placeholder={t("searchPlaceholder")}>
-          <SearchActionMenu
-            actions={filterActions}
-            startIcon={<FilterIcon />}
-            renderedSelectedLabel={tApplication("filteredBy")}
-            renderedDefaultLabel={tApplication("filterByUserStatus")}
-            aria-label={tApplication("filterBy")}
-            multiple
-          />
-        </SearchBar>
-      </Box>
-      <Table
-        total={total}
-        last_page={last_page}
-        setPage={setPage}
-        data={users}
-        columns={columns}
-        queryState={queryState}
-        isPaginated
-      />
+      Subpage for Safe People for Project {id}
     </>
   );
 }
