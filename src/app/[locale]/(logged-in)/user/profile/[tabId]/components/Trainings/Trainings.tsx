5448ff7a64d818081ca173df78588c846bac706a
lewissaunders11 <lewis.saunders@hdruk.ac.uk>
2025-03-10 08:07:33 +0000

    lint

--- src/app/[locale]/(logged-in)/user/profile/[tabId]/components/Trainings/Trainings.tsx
+++ src/app/[locale]/(logged-in)/user/profile/[tabId]/components/Trainings/Trainings.tsx
@@ -10,10 +10,10 @@ import { Box, Link } from "@mui/material";
 import { useTranslations } from "next-intl";
 import { useRouter } from "next/navigation";
 import ProfileNavigationFooter from "@/components/ProfileNavigationFooter";
-import ProfessionalsRegistration from "../ProfessionalRegistrations";
-import Training from "../Training";
 import FormControlCheckbox from "@/components/FormControlCheckbox";
 import { useForm, FormProvider } from "react-hook-form";
+import ProfessionalsRegistration from "../ProfessionalRegistrations";
+import Training from "../Training";
 
 const NAMESPACE_TRANSLATION_PROFILE = "Profile";
 
@@ -27,54 +27,52 @@ export default function Trainings() {
       <PageBodyContainer>
         <PageGuidance {...mockedPersonalDetailsGuidanceProps}>
           <PageBody>
-          <PageSection>
-            <Training />
-          </PageSection>
-          <PageSection>
-            <ProfessionalsRegistration />
-          </PageSection>
+            <PageSection>
+              <Training />
+            </PageSection>
+            <PageSection>
+              <ProfessionalsRegistration />
+            </PageSection>
 
-          <Box sx={{ mt: 1, maxWidth: "50%" }}>
-            <FormControlCheckbox
-              name="accreditedResearcher"
-              label={tProfile("accreditedResearcherCheckboxLabel")}
-              labelCaption={
-                <Link
-                  href={tProfile("accreditedResearcherLinkHref")}
-                  color="primary"
-                  sx={{ display: "block", mt: 0.5 }}
-                >
-                  {tProfile("findOutMore")}
-                </Link>
-              }
-            />
-          </Box>
+            <Box sx={{ mt: 1, maxWidth: "50%" }}>
+              <FormControlCheckbox
+                name="accreditedResearcher"
+                label={tProfile("accreditedResearcherCheckboxLabel")}
+                labelCaption={
+                  <Link
+                    href={tProfile("accreditedResearcherLinkHref")}
+                    color="primary"
+                    sx={{ display: "block", mt: 0.5 }}>
+                    {tProfile("findOutMore")}
+                  </Link>
+                }
+              />
+            </Box>
 
-          <Box sx={{ mt: 1, maxWidth: "50%" }}>
-            <FormControlCheckbox
-              name="userDeclaration"
-              label={tProfile("userDeclarationCheckboxLabel")}
-              labelCaption={
-                <Link
-                  href={tProfile("userDeclarationLinkHref")}
-                  color="primary"
-                  sx={{ display: "block", mt: 0.5 }}
-                >
-                  {tProfile("findOutMore")}
-                </Link>
-              }
-            />
-          </Box>
-          <Box sx={{ display: "flex", justifyContent: "flex-end", mt: 3 }}>
-            <ProfileNavigationFooter
-              nextStepText={tProfile("completeYourProfile")}
-              isLastStep
-              onClick={() => router.push(ROUTES.profileResearcherHome.path)}
-            />
-          </Box>
-        </PageBody>
-      </PageGuidance>
-    </PageBodyContainer>
+            <Box sx={{ mt: 1, maxWidth: "50%" }}>
+              <FormControlCheckbox
+                name="userDeclaration"
+                label={tProfile("userDeclarationCheckboxLabel")}
+                labelCaption={
+                  <Link
+                    href={tProfile("userDeclarationLinkHref")}
+                    color="primary"
+                    sx={{ display: "block", mt: 0.5 }}>
+                    {tProfile("findOutMore")}
+                  </Link>
+                }
+              />
+            </Box>
+            <Box sx={{ display: "flex", justifyContent: "flex-end", mt: 3 }}>
+              <ProfileNavigationFooter
+                nextStepText={tProfile("completeYourProfile")}
+                isLastStep
+                onClick={() => router.push(ROUTES.profileResearcherHome.path)}
+              />
+            </Box>
+          </PageBody>
+        </PageGuidance>
+      </PageBodyContainer>
     </FormProvider>
   );
 }
