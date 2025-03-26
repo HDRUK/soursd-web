dff8c40dc7e21c4142f2c98c0bd6d10b9e6f6f38
lewissaunders11 <lewis.saunders@hdruk.ac.uk>
2025-03-26 07:10:41 +0000

    types

--- src/services/project_details/types.ts
+++ src/services/project_details/types.ts
@@ -10,20 +10,20 @@ type PutProjectDetailsPayload = Partial<ProjectDetails>;
 interface ProjectDetails {
   id: number;
   project_id: number;
-  datasets: string[];
-  other_approval_committees: string[];
-  data_sensitivity_level: string;
-  legal_basis_for_data_article6: string;
+  datasets?: string[];
+  other_approval_committees?: string[];
+  data_sensitivity_level?: string;
+  legal_basis_for_data_article6?: string;
   duty_of_confidentiality: boolean;
   national_data_optout: boolean;
-  request_frequency: RequestFrequency;
-  dataset_linkage_description: string;
-  data_minimisation: string;
-  data_use_description: string;
-  access_date: string;
-  data_privacy: string;
-  research_outputs: string[];
-  data_assets: string;
+  request_frequency?: RequestFrequency;
+  dataset_linkage_description?: string;
+  data_minimisation?: string;
+  data_use_description?: string;
+  access_date?: string;
+  data_privacy?: string;
+  research_outputs?: string[];
+  data_assets?: string;
 }
 
 export enum RequestFrequency {
