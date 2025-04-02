ea14b118e0842cb2454de8ec5cf79b421808c030
Sam Cox <spco@users.noreply.github.com>
2025-04-02 15:55:57 +0100

    fix(SOURSD-932): Fix custodian headers (#242)
    Includes:
    
    * Swap order of affiliations and experience tabs to match design.
    
    * Update tab icons.

--- src/app/[locale]/(logged-in)/data-custodian/profile/components/Users/Users.tsx
+++ src/app/[locale]/(logged-in)/data-custodian/profile/components/Users/Users.tsx
@@ -4,7 +4,7 @@ import Results from "@/components/Results";
 import ResultsCard from "@/components/ResultsCard";
 import { useStore } from "@/data/store";
 import { Link } from "@/i18n/routing";
-import { PageBody, PageSection } from "@/modules";
+import { PageBody, PageBodyContainer, PageSection } from "@/modules";
 import SearchBar from "@/modules/SearchBar";
 import {
   deleteCustodianUser,
@@ -81,129 +81,133 @@ export default function Users() {
   }, []);
 
   return (
-    <PageBody>
-      <PageSection>
-        <Box sx={{ display: "flex", gap: 1, mb: 3 }}>
-          <Box component="form" role="search" sx={{ flexGrow: 1 }}>
-            <SearchBar onSearch={() => {}} />
+    <PageBodyContainer heading={t("users")}>
+      <PageBody>
+        <PageSection>
+          <Box sx={{ display: "flex", gap: 1, mb: 3 }}>
+            <Box component="form" role="search" sx={{ flexGrow: 1 }}>
+              <SearchBar onSearch={() => {}} />
+            </Box>
+            <Button
+              endIcon={<AddCircleOutlineOutlinedIcon />}
+              variant="contained"
+              onClick={() => {
+                if (custodian?.id) {
+                  setModalProps({
+                    open: true,
+                    user: {
+                      first_name: "",
+                      last_name: "",
+                      email: "",
+                      custodian_id: custodian?.id,
+                    },
+                  });
+                }
+              }}>
+              {t("addNewUser")}
+            </Button>
           </Box>
-          <Button
-            endIcon={<AddCircleOutlineOutlinedIcon />}
-            variant="contained"
-            onClick={() => {
-              if (custodian?.id) {
-                setModalProps({
-                  open: true,
-                  user: {
-                    first_name: "",
-                    last_name: "",
-                    email: "",
-                    custodian_id: custodian?.id,
-                  },
-                });
-              }
-            }}>
-            {t("addNewUser")}
-          </Button>
-        </Box>
 
-        <Results
-          noResultsMessage={t("noResults")}
-          errorMessage={t.rich("getError", {
-            contactLink: ContactLink,
-          })}
-          queryState={{
-            isLoading: isGetCustodiansLoading,
-            isError: isGetCustodiansError,
-          }}
-          total={custodiansData?.data?.length}>
-          {custodiansData?.data.map(custodianUser => {
-            const {
-              id,
-              first_name,
-              last_name,
-              email,
-              created_at,
-              user_permissions,
-            } = custodianUser;
+          <Results
+            noResultsMessage={t("noResults")}
+            errorMessage={t.rich("getError", {
+              contactLink: ContactLink,
+            })}
+            queryState={{
+              isLoading: isGetCustodiansLoading,
+              isError: isGetCustodiansError,
+            }}
+            total={custodiansData?.data?.length}>
+            {custodiansData?.data.map(custodianUser => {
+              const {
+                id,
+                first_name,
+                last_name,
+                email,
+                created_at,
+                user_permissions,
+              } = custodianUser;
 
-            const role = user_permissions?.[0]?.permission?.name;
+              const role = user_permissions?.[0]?.permission?.name;
 
-            return (
-              <ResultsCard
-                key={email}
-                icon={
-                  <Icon size="xlarge">
-                    <PersonOutlineOutlinedIcon />
-                  </Icon>
-                }
-                content={
-                  <>
-                    {" "}
-                    <Typography
-                      component={Link}
-                      href={injectParamsIntoPath(
-                        routes.profileCustodianUserById.path,
-                        {
-                          id,
-                        }
-                      )}
-                      variant="h6">
-                      {first_name} {last_name}
-                    </Typography>
-                    {/* Will be read from db */}
-                    <Typography>{role && t(role)}</Typography>
-                  </>
-                }
-                details={
-                  <>
-                    {" "}
-                    <Typography color="caption.main">
-                      {t("addedOn", {
-                        date: formatShortDate(created_at),
-                      })}
-                    </Typography>
-                    <Typography color="caption.main">
-                      {t("lastLoggedIn", {
-                        date: formatShortDate(),
-                      })}
-                    </Typography>
-                  </>
-                }
-                actions={
-                  <>
-                    <IconButton
-                      size="small"
-                      aria-label="Edit user"
-                      color="inherit"
-                      onClick={() =>
-                        setModalProps({
-                          open: true,
-                          user: custodianUser,
-                        })
-                      }>
-                      <CreateOutlinedIcon sx={{ color: "default.main" }} />
-                    </IconButton>
-                    <IconButton
-                      size="small"
-                      aria-label="Delete user"
-                      onClick={() => handleDelete(custodianUser?.id)}>
-                      <DeleteForeverOutlinedIcon sx={{ color: "error.main" }} />
-                    </IconButton>
-                  </>
-                }
-              />
-            );
-          })}
-        </Results>
-        {modalProps?.user && custodian?.id && (
-          <UserModal
-            {...modalProps}
-            custodianId={custodian.id}
-            onClose={handleCloseModal}
-          />
-        )}
-      </PageSection>
-    </PageBody>
+              return (
+                <ResultsCard
+                  key={email}
+                  icon={
+                    <Icon size="xlarge">
+                      <PersonOutlineOutlinedIcon />
+                    </Icon>
+                  }
+                  content={
+                    <>
+                      {" "}
+                      <Typography
+                        component={Link}
+                        href={injectParamsIntoPath(
+                          routes.profileCustodianUserById.path,
+                          {
+                            id,
+                          }
+                        )}
+                        variant="h6">
+                        {first_name} {last_name}
+                      </Typography>
+                      {/* Will be read from db */}
+                      <Typography>{role && t(role)}</Typography>
+                    </>
+                  }
+                  details={
+                    <>
+                      {" "}
+                      <Typography color="caption.main">
+                        {t("addedOn", {
+                          date: formatShortDate(created_at),
+                        })}
+                      </Typography>
+                      <Typography color="caption.main">
+                        {t("lastLoggedIn", {
+                          date: formatShortDate(),
+                        })}
+                      </Typography>
+                    </>
+                  }
+                  actions={
+                    <>
+                      <IconButton
+                        size="small"
+                        aria-label="Edit user"
+                        color="inherit"
+                        onClick={() =>
+                          setModalProps({
+                            open: true,
+                            user: custodianUser,
+                          })
+                        }>
+                        <CreateOutlinedIcon sx={{ color: "default.main" }} />
+                      </IconButton>
+                      <IconButton
+                        size="small"
+                        aria-label="Delete user"
+                        onClick={() => handleDelete(custodianUser?.id)}>
+                        <DeleteForeverOutlinedIcon
+                          sx={{ color: "error.main" }}
+                        />
+                      </IconButton>
+                    </>
+                  }
+                />
+              );
+            })}
+          </Results>
+          {modalProps?.user && custodian?.id && (
+            <UserModal
+              {...modalProps}
+              custodianId={custodian.id}
+              onClose={handleCloseModal}
+            />
+          )}
+        </PageSection>
+      </PageBody>
+    </PageBodyContainer>
   );
 }
