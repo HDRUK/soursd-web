9adf8e59e58468b4f7d3092d10aa7afd918cec3e
Sam Cox <sam.cox@hdruk.ac.uk>
2025-04-08 16:43:51 +0100

    Argh, missed an import

--- src/app/[locale]/(logged-in)/data-custodian/profile/components/UserTrainingAccreditations/UserTrainingAccreditations.tsx
+++ src/app/[locale]/(logged-in)/data-custodian/profile/components/UserTrainingAccreditations/UserTrainingAccreditations.tsx
@@ -1,5 +1,4 @@
 import { useTranslations } from "next-intl";
-import { useStore } from "@/data/store";
 import { PageBodyContainer, PageSection, Training } from "@/modules";
 import { EntityType } from "@/types/api";
 import { Typography } from "@mui/material";
