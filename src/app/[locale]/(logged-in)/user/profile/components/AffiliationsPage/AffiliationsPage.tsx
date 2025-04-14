449f010a3c67b55769a239d7ccf17cdd4235ec20
Peter Hammans <peter.hammans@hdruk.ac.uk>
2025-04-14 15:12:48 +0100

    chore: console log

--- src/app/[locale]/(logged-in)/user/profile/components/AffiliationsPage/AffiliationsPage.tsx
+++ src/app/[locale]/(logged-in)/user/profile/components/AffiliationsPage/AffiliationsPage.tsx
@@ -148,7 +148,6 @@ export default function AffiliationsPage() {
 
   const handleDetailsSubmit = useCallback(
     async (fields: PostAffiliationPayload) => {
-      console.log("**** payload fields", fields);
       if (selectedAffiliation) {
         // Update existing affiliation
         await patchAffiliation({
