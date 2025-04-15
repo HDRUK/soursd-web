3f08d5cdc24b0ceccc10506ad12325b3548b1427
Calum Macdonald <calmacx@gmail.com>
2025-04-15 11:46:51 +0100

    fixes

--- src/hooks/useFallbackTranslations/useFallbackTranslations.test.tsx
+++ src/hooks/useFallbackTranslations/useFallbackTranslations.test.tsx
@@ -1,55 +1,22 @@
-import { renderHook } from "@/utils/testUtils"; // or from '@testing-library/react-hooks'
+import { renderHook } from "@/utils/testUtils";
 import useFallbackTranslations from "./useFallbackTranslations";
 
-jest.mock("next-intl", () => ({
-  useTranslations: jest.fn(),
-}));
-
-import { useTranslations } from "next-intl";
-
 describe("useTranslatedTitle", () => {
-  const mockT = {
-    raw: jest.fn(),
-    // @ts-ignore
-    _: (...args) => args.join("."),
-  };
-
-  beforeEach(() => {
-    jest.clearAllMocks();
-  });
-
   it("returns translated string if available", () => {
-    mockT.raw.mockImplementation(key => `someNamespace.translatedKey`);
-    mockT._ = jest.fn(() => "Translated Value");
-
-    (useTranslations as jest.Mock).mockReturnValue((key: string) => {
-      if (key === "titleKey") return "Translated Value";
-    });
-    (useTranslations as jest.Mock).mockReturnValueOnce(
-      Object.assign((key: string) => "Translated Value", mockT)
-    );
-
     const { result } = renderHook(() =>
-      useFallbackTranslations("someNamespace")
+      useFallbackTranslations("ActionsPanelValidationCheck")
     );
-
-    const translated = result.current("titleKey");
-
-    expect(translated).toBe("Translated Value");
+    const translated = result.current("organisation_aligned_sde_network");
+    expect(translated).toBe(
+      "Is the Organisation aligned with the SDE network?"
+    );
   });
 
   it("returns title-cased key if translation not available", () => {
-    mockT.raw.mockImplementation(key => `someNamespace.${key}`);
-    (useTranslations as jest.Mock).mockReturnValueOnce(
-      Object.assign((key: string) => "", mockT)
-    );
-
     const { result } = renderHook(() =>
-      useFallbackTranslations("someNamespace")
+      useFallbackTranslations("ActionsPanelValidationCheck")
     );
-
-    const fallback = result.current("untranslated_key");
-
-    expect(fallback).toBe("Untranslated Key");
+    const translated = result.current("my_fake_title");
+    expect(translated).toBe("My Fake Title");
   });
 });
