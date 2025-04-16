0c99150acbb2b26959cea065f0b354f205f3c682
Sam Cox <sam.cox@hdruk.ac.uk>
2025-04-16 21:20:38 +0100

    Lint

--- src/app/[locale]/(logged-in)/organisation/profile/user-administration/[subTabId]/[id]/[subSubTabId]/components/SubsTabContents/SubTabsContents.tsx
+++ src/app/[locale]/(logged-in)/organisation/profile/user-administration/[subTabId]/[id]/[subSubTabId]/components/SubsTabContents/SubTabsContents.tsx
@@ -1,10 +1,10 @@
 "use client";
 
 import { notFound } from "next/navigation";
+import UserIdentity from "@/modules/UserIdentity";
 import UserAffiliations from "../../../../../../components/UserAffiliations";
 import UserCustodianOrgInfo from "../../../../../../components/UserCustodianOrgInfo";
 import UserHistory from "../../../../../../components/UserHistory";
-import UserIdentity from "@/modules/UserIdentity";
 import UserProjects from "../../../../../../components/UserProjects";
 import UserTrainingAccreditations from "../../../../../../components/UserTrainingAccreditations";
 import { UserSubTabs } from "../../../../../../consts/tabs";
