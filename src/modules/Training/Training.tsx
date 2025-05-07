872d9c62e453a0940cb36c5ec6a8cafaf4eab0ba
Calum Macdonald <calmacx@gmail.com>
2025-05-07 13:15:52 +0100

    resolve conflict

--- src/modules/Training/Training.tsx
+++ src/modules/Training/Training.tsx
@@ -219,7 +219,7 @@ export default function Training({
         if (updatedHistories) {
           setHistories(updatedHistories);
         }
-      } 
+      } catch (_) {}
     },
     [getHistories, setHistories]
   );
