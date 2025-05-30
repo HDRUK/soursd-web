efe5aff5a9309234c9b404dd82d140b0d99a10cf
peter-hammans <164904644+peter-hammans@users.noreply.github.com>
2025-05-30 13:27:37 +0100

    feat(SOURSD-730): org content (#333)

--- src/app/[locale]/(logged-in)/organisation/profile/components/Home/Home.tsx
+++ src/app/[locale]/(logged-in)/organisation/profile/components/Home/Home.tsx
@@ -10,6 +10,7 @@ import { useTranslations } from "next-intl";
 import ActionLogs from "@/organisms/ActionLogs";
 import { useStore } from "@/data/store";
 import SoursdCard from "@/components/SoursdCard";
+import { mockedOrganisationHomeIntro } from "@/mocks/data/cms";
 
 const NAMESPACE_TRANSLATION_PROFILE = "ProfileOrganisation";
 
@@ -27,29 +28,7 @@ const Home = () => {
                 variant="organisation"
                 panelProps={{
                   heading: "Welcome to SOURSD!",
-                  description: (
-                    <>
-                      You’ll see a list of tasks below we’ve assigned to you to
-                      complete your profile. To help you do that as quickly as
-                      possible here’s a list of things you’ll need before you
-                      dive in:
-                      <ul>
-                        <li>Prerequisite 1</li>
-                        <li>Prerequisite 2</li>
-                        <li>Prerequisite 3</li>
-                      </ul>
-                      <br />
-                      It’s important for your Organisation to nominate and
-                      confirm a SRO (Senior Responsible Officer). The SRO will
-                      be required to get a declaration signed on behalf of their
-                      Organisation and upload this. SOURSD contains information
-                      about your employees and students and their work on
-                      sensitive data projects. The SRO will be responsible for
-                      verifying this information, it’s important that the SRO
-                      has a position of seniority and can be held accountable
-                      for the data.
-                    </>
-                  ),
+                  description: mockedOrganisationHomeIntro,
                 }}
               />
             </PageSection>
