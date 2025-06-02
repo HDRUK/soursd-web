6356c8372679582780a84803cc231ded8e8d0ef5
Peter Hammans <peter.hammans@hdruk.ac.uk>
2025-06-02 09:49:04 +0100

    feat(SOURSD-734): user profile content

--- mocks/data/cms.tsx
+++ mocks/data/cms.tsx
@@ -14,6 +14,19 @@ export const mockedBannerContent = () => (
 export const mockedProjectsIntro =
   "The project list below outlines your projects pending approval, including validated Users and Organisations. Clicking on a project will show you more information and next steps.";
 
+export const mockedUserHomeIntro = (
+  <>
+    To complete your profile, please find a list of tasks assigned to you below.
+    Before getting started, make sure you have the following ready:
+    <ul>
+      <li>A form of ID (e.g. passport or driving licence)</li>
+      <li>Your ORCiD</li>
+      <li>An up-to-date CV</li>
+      <li>Any relevant training certificates or professional member IDs</li>
+    </ul>
+  </>
+);
+
 export const mockedPendingAffiliations = (
   <>
     <Typography mb={2}>
