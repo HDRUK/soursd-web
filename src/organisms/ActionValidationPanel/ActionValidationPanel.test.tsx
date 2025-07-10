38d2cd96f41c52b61805c4eadcf66996201970e0
Calum Macdonald <calmacx@gmail.com>
2025-07-10 14:08:17 +0100

    what was that

--- src/organisms/ActionValidationPanel/ActionValidationPanel.test.tsx
+++ src/organisms/ActionValidationPanel/ActionValidationPanel.test.tsx
@@ -201,6 +201,7 @@ describe("<ActionValidationPanel/>", () => {
     expect(viewMoreBox).toBeInTheDocument();
 
     const items = viewMoreBox.children;
+
     expect(items.length).toBe(3);
 
     const viewMoreButton = screen.getByTestId("view-more-button");
