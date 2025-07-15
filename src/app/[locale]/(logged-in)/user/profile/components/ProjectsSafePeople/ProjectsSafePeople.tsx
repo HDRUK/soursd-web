1eebd253887469b979924c760fbe338e3f789598
Calum Macdonald <69473770+calmacx@users.noreply.github.com>
2025-07-15 14:33:10 +0100

    fix(REGISTRY-1496): Updating safe people views for users and organisations (#388)

--- src/app/[locale]/(logged-in)/user/profile/components/ProjectsSafePeople/ProjectsSafePeople.tsx
+++ src/app/[locale]/(logged-in)/user/profile/components/ProjectsSafePeople/ProjectsSafePeople.tsx
@@ -3,20 +3,36 @@
 import { useStore } from "@/data/store";
 import PageBody from "@/modules/PageBody";
 import ProjectUsersList from "@/organisms/ProjectUsersList";
+import { useGetProjectUsers } from "@/services/projects";
 import { EntityType } from "@/types/api";
 
 export default function ProjectsSafePeople() {
-  const { projectId, route } = useStore(state => ({
+  const { registryId, projectId, route } = useStore(state => ({
+    registryId: state.getUser()?.registry_id,
     projectId: state.getCurrentProject().id,
     route: state.getApplication().routes.profileCustodianUsersProjects,
   }));
 
+  // filter to only display the current user via registry ID, if in this project
+  // note - do we really want this?
+  const { data, total, last_page, page, setPage } = useGetProjectUsers(
+    projectId,
+    {
+      defaultQueryParams: { registry_id: registryId },
+    }
+  );
+
   return (
     <PageBody>
       <ProjectUsersList
+        data={data}
+        total={total}
+        last_page={last_page}
+        page={page}
+        setPage={setPage}
+        isPaginated
         variant={EntityType.USER}
-        custodianId={1}
-        projectId={projectId}
+        includeColumns={["name", "projectRole", "organisationName"]}
         routes={{
           name: route,
         }}
