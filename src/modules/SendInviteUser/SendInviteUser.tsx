267f11a1a34ca510875bf0fc43e7e4fd23f315fc
Calum Macdonald <calmacx@gmail.com>
2025-07-17 16:17:03 +0100

    revert file

--- src/modules/SendInviteUser/SendInviteUser.tsx
+++ src/modules/SendInviteUser/SendInviteUser.tsx
@@ -3,24 +3,43 @@ import ErrorMessage from "@/components/ErrorMessage";
 import InviteUser from "../InviteUser";
 import useUserInvite from "../../queries/useUserInvite";
 import { showAlert } from "../../utils/showAlert";
-import { useState } from "react";
 
 const NAMESPACE_TRANSLATIONS_ORGANISATION = "User";
 
 interface SendInviteUserProps {
-  forceSelectOrganisation?: boolean;
   organisationId?: number;
   onSuccess?: () => void;
   onError?: () => void;
 }
 
 export default function SendInviteUser({
-  forceSelectOrganisation = false,
   organisationId,
   onSuccess,
   onError,
 }: SendInviteUserProps) {
   const t = useTranslations(NAMESPACE_TRANSLATIONS_ORGANISATION);
 
-  return <InviteUser onSuccess={onSuccess} />;
+  const handleErrorAlert = () => {
+    showAlert("error", {
+      text: <ErrorMessage t={t} tKey="inviteUserError" />,
+      confirmButtonText: t("inviteUserErrorButton"),
+      willClose: () => onError?.(),
+    });
+  };
+
+  const handleSuccessAlert = () => {
+    showAlert("success", {
+      text: t("inviteUserSuccess"),
+      confirmButtonText: t("inviteUserSuccessButton"),
+      willClose: () => onSuccess?.(),
+    });
+  };
+
+  const { queryState, handleSubmit } = useUserInvite({
+    organisationId,
+    onError: handleErrorAlert,
+    onSuccess: handleSuccessAlert,
+  });
+
+  return <InviteUser onSubmit={handleSubmit} queryState={queryState} />;
 }
