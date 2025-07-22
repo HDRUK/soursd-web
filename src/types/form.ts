005bca598b067daed77029856cd47c14b326c841
Calum Macdonald <calmacx@gmail.com>
2025-07-22 10:43:51 +0100

    fix

--- src/types/form.ts
+++ src/types/form.ts
@@ -6,8 +6,10 @@ interface QueryState {
   isFetching?: boolean;
   isLoading?: boolean;
   reset?: () => void;
+  status?: string;
 }
 interface MutationState {
+  fetchStatus?: string;
   isError: boolean;
   error?: unknown | string | null;
   isSuccess?: boolean;
