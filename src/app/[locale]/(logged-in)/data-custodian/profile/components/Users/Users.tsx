0fa07414c94e1b2d5d93f0b3b87ff9cd9b30f324
Wendi Bacon <44605769+nomadscientist@users.noreply.github.com>
2025-08-15 10:57:12 +0100

    fix

--- src/app/[locale]/(logged-in)/data-custodian/profile/components/Users/Users.tsx
+++ src/app/[locale]/(logged-in)/data-custodian/profile/components/Users/Users.tsx
@@ -7,7 +7,7 @@ import PageBodyContainer from "@/modules/PageBodyContainer";
 import PageSection from "@/modules/PageSection";
 import ProjectUsers from "@/organisms/ProjectUsers";
 import { EntityType } from "@/types/api";
-import { Markdown } from "@/components/Markdown";
+import Markdown from "@/components/Markdown";
 import { useTranslations } from "next-intl";
 
 const NAMESPACE_TRANSLATIONS_PROFILE = "CustodianProfile";
