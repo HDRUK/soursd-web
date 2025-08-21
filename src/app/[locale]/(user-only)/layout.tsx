c9aec1ea2ce04fcd0839dfa6018efaabdf5476ae
Phil Reeks <phil.reeks@hdruk.ac.uk>
2025-08-21 13:21:20 +0100

    Remove application redirects from register page

--- src/app/[locale]/(user-only)/layout.tsx
+++ src/app/[locale]/(user-only)/layout.tsx
@@ -1,11 +1,8 @@
-import useApplicationRedirects from "@/hooks/useApplicationRedirects";
 import { PageContainer } from "@/modules";
 import ApplicationUser from "@/organisms/ApplicationUser";
 import { PropsWithChildren } from "react";
 
 export default async function Layout({ children }: PropsWithChildren) {
-  await useApplicationRedirects();
-
   return (
     <ApplicationUser>
       <PageContainer>{children}</PageContainer>
